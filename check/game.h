50b2a8020d012e851895ad703cd1a040780888cd
David Meybohm <dmeybohm@gmail.com>
2003-08-30 08:15:19 +0000

    Update game_load() args

--- check/game.h
+++ check/game.h
@@ -17,7 +17,7 @@ struct game *game_new   (color_t turn, color_t computer_player);
 void         game_free  (struct game *game);
 
 int          game_save  (struct game *game);
-struct game *game_load  (const char *file, color_t player);
+struct game *game_load  (color_t player);
 void         game_move  (struct game *game, struct move *move);
 
 #endif /* EVOLVE_CHESS_GAME_H_ */
