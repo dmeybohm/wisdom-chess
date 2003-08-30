9ca30dde1a1ea99eaf0cb40f89e45fed4c5f1531
David Meybohm <dmeybohm@gmail.com>
2003-08-30 03:48:31 +0000

    Use external def struct move_tree

--- check/game.h
+++ check/game.h
@@ -1,11 +1,14 @@
 #ifndef EVOLVE_CHESS_GAME_H_
 #define EVOLVE_CHESS_GAME_H_
 
+struct board;
+struct move_tree;
+
 struct game
 {
-	struct board    *board;
-	move_tree_t     *history;
-	color_t          turn;
+	struct board      *board;
+	struct move_tree  *history;
+	color_t            turn;
 };
 
 #endif /* EVOLVE_CHESS_GAME_H_ */
