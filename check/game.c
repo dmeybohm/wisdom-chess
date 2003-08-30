d7cb25276bca9e8fa1642fe02a908babca3de75d
David Meybohm <dmeybohm@gmail.com>
2003-08-30 06:27:58 +0000

    Add game_{load,save,move} and game->player

--- check/game.c
+++ check/game.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "piece.h"
 #include "board.h"
+#include "move.h"
 #include "move_tree.h"
-
+#include "str.h"
 #include "game.h"
 
-struct game *game_new (color_t turn)
+struct game *game_new (color_t turn, color_t computer_player)
 {
 	struct game *game;
 
@@ -20,10 +22,15 @@ struct game *game_new (color_t turn)
 	game->history = NULL;
 
 	/* if 'turn' is something bogus, use white */
-	if (turn != COLOR_WHITE && turn != COLOR_BLACK)
+	if (is_color_invalid (turn))
 		turn = COLOR_WHITE;
 
-	game->turn = turn;
+	/* if 'computer_player' is bogus, use black */
+	if (is_color_invalid (computer_player))
+		computer_player = COLOR_BLACK;
+
+	game->player = computer_player;
+	game->turn   = turn;
 
 	return game;
 }
@@ -38,4 +45,80 @@ void game_free (struct game *game)
 
 	free (game);
 }
+
+void game_move (struct game *game, move_t *move)
+{
+	/* add this move to the history */
+	game->history = move_tree_new (game->history, *move);
+
+	/* do the move */
+	do_move (game->board, game->turn, move);
+
+	/* take our turn */
+	game->turn = color_invert (game->turn);
+}
+
+int game_save (struct game *game)
+{
+	FILE *out;
+	char buf[128];
+	move_tree_t *history;
+
+	printf ("save to what file? ");
+	fflush (stdout);
+
+	if (fgets (buf, sizeof (buf)-1, stdin) == NULL)
+		return 0;
+
+	chomp (buf);
+
+	if (!(out = fopen (buf, "w+")))
+	{
+		printf ("error opening file: %s", strerror (errno));
+		return 0;
+	}
+
+	history = game->history;
+
+	while (!is_null_move (history->move))
+	{
+		fprintf (out, "%s\n", move_str (history->move));
+		history = history->parent;
+	}
+
+	fclose (out);
+
+	return 1; /* need to check for failure here */
+}
+
+struct game *game_load (const char *file, color_t player)
+{
+	struct game *game;
+	char buf[128];
+	FILE *f;
+	move_t move;
+
+	if (!(f = fopen (file, "r")))
+		return NULL;
+
+	game = game_new (player, COLOR_NONE);
+
+	while (fgets (buf, sizeof (buf) - 1, f) != NULL)
+	{
+		if (!move_parse (buf, game->turn, &move))
+		{
+			printf ("Error parsing game file %s: invalid move \"%s\"",
+					file, buf);
+			game_free (game);
+			return NULL;
+		}
+
+		game_move (game, &move);
+	}
+
+	fclose (f);
+	
+	return game;
+}
+
 /* vi: set ts=4 sw=4: */
--- check/game.h
+++ check/game.h
@@ -3,12 +3,21 @@
 
 struct board;
 struct move_tree;
+struct move;
 
 struct game
 {
 	struct board      *board;
 	struct move_tree  *history;
+	color_t            player;   /* side the computer is playing as */
 	color_t            turn;
 };
+
+struct game *game_new   (color_t turn, color_t computer_player);
+void         game_free  (struct game *game);
+
+int          game_save  (struct game *game);
+struct game *game_load  (const char *file, color_t player);
+void         game_move  (struct game *game, struct move *move);
 
 #endif /* EVOLVE_CHESS_GAME_H_ */
