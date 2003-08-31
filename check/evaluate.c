2de95749e2014667bda11538e8ad485b558f5665
David Meybohm <dmeybohm@gmail.com>
2003-08-31 03:17:57 +0000

    use material.c for keeping track of piece value

--- check/evaluate.c
+++ check/evaluate.c
@@ -6,10 +6,22 @@
 #include "piece.h"
 #include "check.h"
 #include "search.h"
+#include "material.h"
+
 #include "debug.h"
 
 DEFINE_DEBUG_CHANNEL (evaluate, 0);
 
+/*
+ * We use material.c for calculating the score after each move
+ * is made. That should be less expensive overall, but it may
+ * not be a good idea, because the do_move/undo_move path is
+ * very critical. A transposition table would probably make
+ * it even more so..
+ */
+#define USE_MATERIAL_STRUCT_INSTEAD 1
+
+#ifndef USE_MATERIAL_STRUCT_INSTEAD
 int const piece_weights[] =
 {
 	/* PIECE_NONE   */     0,
@@ -20,19 +32,26 @@ int const piece_weights[] =
 	/* PIECE_KNIGHT */     320,
 	/* PIECE_PAWN   */     100,
 };
+#endif
+
+#define CASTLE_POSITIVE_WEIGHT        60 /* trying to catch a bug */
+#define CASTLE_NEGATIVE_WEIGHT        60 /* trying to catch a bug */
 
 /* stupid piece player 
  * this could be improved by just keeping the piece count in the board */
 int evaluate (struct board *board, color_t who, int examine_checkmate, 
               move_t *move)
 {
-	piece_t    piece;
-	int        row, col;
 	int        score;
+#ifndef USE_MATERIAL_STRUCT_INSTEAD
 	signed int piece_type;
+	int        row, col;
+	piece_t    piece;
+#endif
 
 	/* this makes white -> -1, black -> 1 */
 	const int  direction  = PAWN_DIRECTION (who);
+	const int  opponent   = -direction;
 
 	DBG (evaluate, "direction = %d\n", direction);
 	score = 0;
@@ -43,9 +62,24 @@ int evaluate (struct board *board, color_t who, int examine_checkmate,
 #endif
 
 	if (board->castled[who] == CASTLE_CASTLED)
-		score += 60;
-	else if (!able_to_castle (board, who, CASTLE_KINGSIDE | CASTLE_QUEENSIDE))
-		score -= 40;
+		score += CASTLE_POSITIVE_WEIGHT;
+	else 
+	{
+		if (!able_to_castle (board, who, CASTLE_KINGSIDE))
+			score -= CASTLE_NEGATIVE_WEIGHT;
+		if (!able_to_castle (board, who, CASTLE_QUEENSIDE))
+			score -= CASTLE_NEGATIVE_WEIGHT;
+	}
+
+	if (board->castled[opponent] == CASTLE_CASTLED)
+		score -= CASTLE_NEGATIVE_WEIGHT;
+	else
+	{
+		if (!able_to_castle (board, opponent, CASTLE_KINGSIDE))
+			score += CASTLE_POSITIVE_WEIGHT;
+		if (!able_to_castle (board, opponent, CASTLE_QUEENSIDE))
+			score += CASTLE_POSITIVE_WEIGHT;
+	}
 
 	if (is_king_threatened (board, who, ROW    (board->king_pos[who]),
 	                                    COLUMN (board->king_pos[who])))
@@ -62,6 +96,8 @@ int evaluate (struct board *board, color_t who, int examine_checkmate,
 	}
 #endif
 
+	/* obsoleted by struct material */
+#ifndef USE_MATERIAL_STRUCT_INSTEAD
 	for_each_position (row, col)
 	{
 #if 1
@@ -82,12 +118,15 @@ int evaluate (struct board *board, color_t who, int examine_checkmate,
 			score -= piece_weights[PIECE_TYPE (piece)];
 #endif
 	}
+#endif /* USE_MATERIAL */
 
+	score += material_score (board->material, who);
+
+#if 0
 	if (move)
 		DBG (evaluate, "move = %s\n", move_str (*move));
 	DBG (evaluate, "score = %d\n", score);
 	DBG (evaluate, "player = %s\n", who == COLOR_WHITE ?"white":"black");
-#if 0
 	board_print (board);
 #endif
 
