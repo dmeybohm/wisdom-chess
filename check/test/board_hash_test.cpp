ef6a43656b86b084dabbe92a03fd6333354597c7
David Meybohm <dmeybohm@gmail.com>
2020-10-01 21:35:35 -0400

    Remove dummy test cases

--- check/test/board_hash_test.cpp
+++ check/test/board_hash_test.cpp
@@ -7,22 +7,6 @@ extern "C" {
 #include "../board.h"
 }
 
-int Factorial( int number ) {
-    //    return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-    return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
-}
-
-TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
-
-TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
-
 TEST_CASE( "Board hashes can be initialized", "[single-file]" ) {
     struct board_hash board_hash;
     struct board *board = board_new();
@@ -40,7 +24,7 @@ TEST_CASE( "Board hashes return to the same hash with a normal move", "[single-f
 
     board_hash.hash = 0;
 
-    board_hash_init(&board_hash, board);
+    board_hash_init (&board_hash, board);
 
     // move a pawn.
     move_t mv = move_create (1, 4, 3, 4);
