4432e5c29a15b400692c46c2328705ef418bdaec
David Meybohm <dmeybohm@gmail.com>
2020-10-10 14:39:40 -0400

    Remove optional virtual keyword

--- check/test/board_builder.hpp
+++ check/test/board_builder.hpp
@@ -44,7 +44,7 @@ class board_builder_exception : public exception
 
 public:
     explicit board_builder_exception (const char *message) { this->message = message; }
-    virtual const char *what() const noexcept { return this->message; }
+    const char *what() const noexcept { return this->message; }
 };
 
 #endif //WIZDUMB_BOARD_BUILDER_HPP
