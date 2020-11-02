6c3d29638374d8ffcaf2aa4cadb62e10b1efb3c6
David Meybohm <dmeybohm@gmail.com>
2020-11-02 00:27:11 -0500

    Add extra test

--- check/test/piece_test.cpp
+++ check/test/piece_test.cpp
@@ -7,7 +7,7 @@ extern "C"
 
 TEST_CASE( "A piece can be converted", "[piece]" )
 {
-    auto color = GENERATE (COLOR_WHITE, COLOR_BLACK);
+    auto color = GENERATE (COLOR_NONE, COLOR_WHITE, COLOR_BLACK);
     auto piece = GENERATE (PIECE_NONE, PIECE_PAWN, PIECE_BISHOP, PIECE_KNIGHT, PIECE_ROOK, PIECE_QUEEN, PIECE_KING);
 
     piece_t combined = MAKE_PIECE (color, piece);
