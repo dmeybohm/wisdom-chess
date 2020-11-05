66e77b020859afd17dd63ae226b87176f06b0b6b
David Meybohm <dmeybohm@gmail.com>
2020-11-04 22:57:06 -0500

    Revert "Switch is_king_threatened to use attack vectors"
    This reverts commit 47358b7a

--- check/src/check.c
+++ check/src/check.c
@@ -32,7 +32,144 @@ bool is_checkmated (struct board *board, color_t who)
 bool is_king_threatened (struct board *board, color_t who,
                          uint8_t king_row, uint8_t king_col)
 {
-    return attack_vector_count (&board->attacks, color_invert(who), coord_create (king_row, king_col));
+	int           row, col;
+	piece_t       what;
+	int           c_dir, r_dir;
+	move_list_t  *kt_moves;
+	move_t       *move;
+
+	/* check each side of the king's row */
+	col = king_col;
+	for (r_dir = -1; r_dir <= 1; r_dir += 2)
+	{
+		for (row = NEXT (king_row, r_dir); VALID (row); row = NEXT (row, r_dir))
+		{
+			what = PIECE_AT (board, row, col);
+
+			if (PIECE_TYPE (what) == PIECE_NONE)
+				continue;
+
+			if (PIECE_COLOR (what) == who)
+				break;
+
+			if (PIECE_TYPE (what) == PIECE_ROOK || 
+				PIECE_TYPE (what) == PIECE_QUEEN)
+				return true;
+
+			break;
+		}
+	}
+
+	/* check each side of the king's column */
+	row = king_row;
+	for (c_dir = -1; c_dir <= 1; c_dir += 2)
+	{
+		for (col = NEXT (king_col, c_dir); VALID (col); col = NEXT (col, c_dir))
+		{
+			what = PIECE_AT (board, row, col);
+
+			if (PIECE_TYPE (what) == PIECE_NONE)
+				continue;
+
+			if (PIECE_COLOR (what) == who)
+				break;
+
+			if (PIECE_TYPE (what) == PIECE_ROOK || 
+				PIECE_TYPE (what) == PIECE_QUEEN)
+				return true;
+
+			break;
+		}
+	}
+
+	/* check each diagonal direction */
+	for (r_dir = -1; r_dir <= 1; r_dir += 2)
+	{
+		for (c_dir = -1; c_dir <= 1; c_dir += 2)
+		{
+			for (row = NEXT (king_row, r_dir), col = NEXT (king_col, c_dir); 
+				 VALID (row) && VALID (col); 
+                 row = NEXT (row, r_dir), col = NEXT (col, c_dir))
+			{
+				what = PIECE_AT (board, row, col);
+
+				if (PIECE_TYPE (what) == PIECE_NONE)
+					continue;
+
+				if (PIECE_COLOR (what) == who)
+					break;
+
+				if (PIECE_TYPE (what) == PIECE_BISHOP || 
+				    PIECE_TYPE (what) == PIECE_QUEEN)
+				{
+					return true;
+				}
+
+				break;
+			}
+		}
+	}
+
+	/* check for knight checks */
+	kt_moves = generate_knight_moves (king_row, king_col);
+
+	for_each_move (move, kt_moves)
+	{
+		coord_t dst;
+
+		dst = MOVE_DST (*move);
+
+		row = ROW (dst);
+		col = COLUMN (dst);
+
+		what = PIECE_AT (board, row, col);
+
+		if (PIECE_TYPE (what) == PIECE_NONE)
+			continue;
+
+		if (PIECE_TYPE (what) == PIECE_KNIGHT && PIECE_COLOR (what) != who)
+			return true;
+	}
+	
+	/* check for pawn checks */
+	r_dir = PAWN_DIRECTION (who);
+
+	for (c_dir = -1; c_dir <= 1; c_dir += 2)
+	{
+		row = NEXT (king_row, r_dir);
+		col = NEXT (king_col, c_dir);
+
+		if (INVALID (row) || INVALID (col))
+			continue;
+
+		what = PIECE_AT (board, row, col);
+
+		if (PIECE_TYPE (what) == PIECE_PAWN && PIECE_COLOR (what) != who)
+			return true;
+	}
+
+	/* check for king checks */
+	for (row = king_row-1; row <= king_row+1; row++)
+	{
+		if (INVALID (row))
+			continue;
+
+		for (col = king_col-1; col <= king_col+1; col++)
+		{
+			if (INVALID (col))
+				continue;
+
+			if (col == king_col && row == king_row)
+				continue;
+
+			what = PIECE_AT (board, row, col);
+			
+			if (PIECE_TYPE (what) == PIECE_KING && PIECE_COLOR (what) != who)
+				return true;
+		}
+	}
+
+	return false;
 }
 
 bool was_legal_move (struct board *board, color_t who, move_t *mv)
