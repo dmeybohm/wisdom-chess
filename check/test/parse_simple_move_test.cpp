f2d8cb9b98f1bec8acd7ba211723d8e5c96f80b7
David Meybohm <dmeybohm@gmail.com>
2020-11-08 18:46:38 -0500

    CHange CHECK to REQUIRE

--- check/test/parse_simple_move_test.cpp
+++ check/test/parse_simple_move_test.cpp
@@ -6,8 +6,8 @@ TEST_CASE("parse_simple_move parses captures and non-captures", "[parse-simple-m
     move_t capture = parse_simple_move("a6xb7");
     move_t non_capture = parse_simple_move("e4 e8");
 
-    CHECK( move_equals (capture, move_parse ("a6xb7", COLOR_WHITE)) );
-    CHECK( move_equals (non_capture, move_parse ("e4 e8", COLOR_WHITE)) );
+    REQUIRE( move_equals (capture, move_parse ("a6xb7", COLOR_WHITE)) );
+    REQUIRE( move_equals (non_capture, move_parse ("e4 e8", COLOR_WHITE)) );
 }
 
 TEST_CASE("parse_simple_move throws an exception for en-passant moves", "[parse-simple-move]")
