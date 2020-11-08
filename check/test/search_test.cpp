455ab87d0577db8ca1148f715ac0c674f0c8834d
David Meybohm <dmeybohm@gmail.com>
2020-11-08 15:21:17 -0500

    Add test for mate in one and a half from wont-checkmate.gam

--- check/test/search_test.cpp
+++ check/test/search_test.cpp
@@ -36,10 +36,52 @@ TEST_CASE( "Can find mate in 3", "[search]" )
     int score = search (board, COLOR_WHITE, 4, 0, &result,
                         -INFINITE, INFINITE, 0,
                         &variation, 0, &large_timer, nullptr);
+    move_tree_destroy (variation);
 
     CHECK( !move_equals (result, move_null ()) );
 
     int equal_result = move_equals (result, move_parse ("f6 a6", COLOR_WHITE));
+    CHECK( equal_result != 0 );
+    CHECK( score == INFINITE );
+}
+
+//
+// This position has multiple mating chances, as well as stalements, so this will test if the
+// search can find the most efficient mate.
+//
+// Mating moves: ... Rd4+  2. Ke5 f6#
+//
+TEST_CASE( "Can find mate in 1 1/2", "[search]" )
+{
+    board_builder builder;
+    struct timer large_timer {};
+    timer_init (&large_timer, 30);
+
+    builder.add_piece ("c8", COLOR_BLACK, PIECE_BISHOP);
+    builder.add_piece ("e8", COLOR_BLACK, PIECE_KNIGHT);
+    builder.add_piece ("c7", COLOR_BLACK, PIECE_KING);
+    builder.add_piece ("f7", COLOR_BLACK, PIECE_PAWN);
+    builder.add_piece ("a6", COLOR_BLACK, PIECE_PAWN);
+    builder.add_piece ("g6", COLOR_BLACK, PIECE_PAWN);
+    builder.add_piece ("c5", COLOR_BLACK, PIECE_PAWN);
+    builder.add_piece ("b4", COLOR_BLACK, PIECE_ROOK);
+    builder.add_piece ("b3", COLOR_BLACK, PIECE_KNIGHT);
+
+    builder.add_piece ("d5", COLOR_WHITE, PIECE_KING);
+
+    struct board *board = builder.build();
+
+    move_t result;
+    move_tree_t *variation = nullptr;
+    int score = search (board, COLOR_BLACK, 4, 0, &result,
+                        -INFINITE, INFINITE, 0,
+                        &variation, 0, &large_timer, nullptr);
+
+    print_reverse_recur (variation);
+    move_tree_destroy (variation);
+    CHECK( !move_equals (result, move_null ()) );
+
+    int equal_result = move_equals (result, move_parse ("b4 d4", COLOR_BLACK));
     CHECK( equal_result != 0 );
     CHECK( score == INFINITE );
 }
