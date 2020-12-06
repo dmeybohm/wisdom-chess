5c0b6a890ae1a543b88a20b0da645b29b132398f
David Meybohm <dmeybohm@gmail.com>
2020-12-06 12:09:30 -0500

    In-progress on board code implementation and rename some variables

--- check/test/board_code_test.cpp
+++ check/test/board_code_test.cpp
@@ -14,12 +14,20 @@ TEST_CASE( "Board code is able to be set", "[board-code]" )
     REQUIRE( num_zeroes == initial_str.size () );
 
     coord_t a8 = coord_parse ("a8");
-    piece_t black_king = make_piece (Color::Black, Piece::Pawn);
-    code.add_piece (a8, black_king);
+    piece_t black_pawn = make_piece (Color::Black, Piece::Pawn);
+    code.add_piece (a8, black_pawn);
 
     REQUIRE( code != initial );
 
     code.remove_piece (a8);
 
     REQUIRE ( code == initial );
+
+    coord_t h1 = coord_parse ("h1");
+    piece_t white_king = make_piece (Color::White, Piece::King);
+    code.add_piece (h1, white_king);
+
+    std::string result = code.to_string();
+    result = result.substr (0, 4);
+    REQUIRE( result == "0001" );
 }
