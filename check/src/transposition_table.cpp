8ff7360ae5316ab348ef45ef0d0e1be18b20aa79
David Meybohm <dmeybohm@gmail.com>
2021-03-26 19:15:38 -0400

    Don't multiple code when adding color, multiply the score.

--- check/src/transposition_table.cpp
+++ check/src/transposition_table.cpp
@@ -1,6 +1,10 @@
 #include "transposition_table.hpp"
 #include "board.hpp"
 
+#include <unordered_set>
+#include <functional>
+#include <map>
+
 namespace wisdom
 {
     std::optional<Transposition> TranspositionTable::lookup (BoardHashCode hash, Color who)
@@ -24,7 +28,7 @@ namespace wisdom
     {
         verify();
 
-        auto map_iterator = my_map.find (transposition.code);
+        auto map_iterator = my_map.find (transposition.hash_code);
         if (map_iterator != my_map.end ())
         {
             my_list.erase (map_iterator->second);
@@ -35,7 +39,7 @@ namespace wisdom
 
         transposition = transposition.with_color (who);
         my_list.push_front (transposition);
-        my_map.insert (make_pair (transposition.code, my_list.begin ()));
+        my_map.insert (make_pair (transposition.hash_code, my_list.begin ()));
         my_num_elements++;
 
         if (my_num_elements > Max_Transpositions)
@@ -50,24 +54,45 @@ namespace wisdom
     {
         auto last_iterator = my_list.end();
         last_iterator--;
-        my_map.erase (last_iterator->code);
+        my_map.erase (last_iterator->hash_code);
         my_list.pop_back ();
         my_num_elements--;
     }
 
     void TranspositionTable::verify () const
     {
+#if 0
+        {
+            std::unordered_set<Transposition> list_uniques;
+            int cnt = 0;
+            std::map<int, Transposition> dupes;
+            for (Transposition t : my_list)
+            {
+                if (list_uniques.contains (t))
+                {
+                    dupes[cnt] = t;
+                }
+                list_uniques.insert (t);
+                cnt++;
+            }
+            if (dupes.size() > 0)
+            {
+                assert(0);
+            }
+        }
+
         if (static_cast<size_t>(my_map.size ()) != static_cast<size_t>(my_list.size ()))
             assert (my_map.size () == my_list.size ());
         if (my_map.size () != my_num_elements)
             assert (my_map.size () == my_num_elements);
         if (my_list.size () != my_num_elements)
             assert (my_list.size () == my_num_elements);
+#endif
     }
 
     Transposition::Transposition (const Board &board, int _score) :
-        code { board.code.hash_code() },
-        score { _score }
+            hash_code { board.code.hash_code () },
+            score { _score }
     {
     }
 }
--- check/src/transposition_table.hpp
+++ check/src/transposition_table.hpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <unordered_map>
 #include <optional>
+#include <functional>
 
 #include "board_code.hpp"
 
@@ -11,20 +12,28 @@ namespace wisdom
 {
     struct Transposition
     {
-        BoardHashCode code;
+        BoardHashCode hash_code;
         int score;
 
         Transposition () = default;
+        Transposition (const Transposition &other) = default;
+
         Transposition (const Board &board, int _score);
         Transposition (BoardHashCode _code, int _score) :
-            code { _code }, score { _score } {}
+                hash_code { _code }, score { _score } {}
+
+        bool operator==(const Transposition &other) const
+        {
+            return other.hash_code == this->hash_code;
+        }
 
         Transposition with_color (Color who)
         {
             Transposition result { *this };
-            result.code *= who == Color::Black ? -1 : 1;
+            result.score *= who == Color::Black ? -1 : 1;
             return result;
         }
+
     };
 
     using TranspositionList = std::list<Transposition>;
@@ -56,6 +65,23 @@ namespace wisdom
             return my_num_elements;
         }
     };
+
+
+
 }
+
+namespace std
+{
+    template<>
+    struct hash<wisdom::Transposition>
+    {
+        std::size_t operator()(const wisdom::Transposition &transposition) const
+        {
+            return transposition.hash_code;
+        }
+    };
+}
+
+
 
 #endif //WISDOM_TRANSPOSITION_TABLE_HPP
