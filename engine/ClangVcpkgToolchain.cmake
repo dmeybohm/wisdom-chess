7bce775e223570b63ef4fc8da6407d1e72887112
David Meybohm <dmeybohm@gmail.com>
2021-06-27 14:58:36 -0400

    Add toolchain for clang/vcpkg for dbg sqlite

---
+++ engine/ClangVcpkgToolchain.cmake
@@ -0,0 +1,4 @@
+# See https://cmake.org/pipermail/cmake/2018-November/068531.html for explanation:
+include(${CMAKE_CURRENT_LIST_DIR}/ClangToolchain.cmake)
+
+include(/usr/local/vcpkg/scripts/buildsystems/vcpkg.cmake)
