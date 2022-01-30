dd0e929437921de1323e69244322963ed543cfa4
David Meybohm <dmeybohm@gmail.com>
2022-01-30 12:10:10 -0500

    Only randomize before setup

--- engine/benchmark/is_king_threatened.cpp
+++ engine/benchmark/is_king_threatened.cpp
@@ -22,16 +22,13 @@ static void benchmark_is_black_king_threatened (benchmark::State& state)
     using namespace wisdom;
 
     Board board;
+    board.randomize_positions ();
+    auto king_pos = board.get_king_position (Color::Black);
+    auto king_row = Row (king_pos);
+    auto king_col = Column (king_pos);
 
     for (auto _ : state)
     {
-        state.PauseTiming ();
-        board.randomize_positions ();
-        auto king_pos = board.get_king_position (Color::Black);
-        auto king_row = Row (king_pos);
-        auto king_col = Column (king_pos);
-        state.ResumeTiming ();
-
         benchmark::DoNotOptimize(is_king_threatened (board, Color::White, king_row, king_col));
     }
 }
@@ -41,16 +38,13 @@ static void benchmark_is_white_king_threatened (benchmark::State& state)
     using namespace wisdom;
 
     Board board;
+    board.randomize_positions ();
+    auto king_pos = board.get_king_position (Color::Black);
+    auto king_row = Row (king_pos);
+    auto king_col = Column (king_pos);
 
     for (auto _ : state)
     {
-        state.PauseTiming ();
-        board.randomize_positions ();
-        auto king_pos = board.get_king_position (Color::Black);
-        auto king_row = Row (king_pos);
-        auto king_col = Column (king_pos);
-        state.ResumeTiming ();
-
         benchmark::DoNotOptimize(is_king_threatened (board, Color::Black, king_row, king_col));
     }
 }
