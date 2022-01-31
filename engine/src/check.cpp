f25c4fb19804cb213b086bc931efe1ceac511c53
David Meybohm <dmeybohm@gmail.com>
2022-01-30 20:02:25 -0500

    1 nanosecond faster...sheesh

--- engine/src/check.cpp
+++ engine/src/check.cpp
@@ -167,8 +167,8 @@ namespace wisdom
         const Board& my_board;
         int my_king_row;
         int my_king_col;
-        Color my_king_color;
-        Color my_opponent_color;
+        int my_king_color_index;
+        int my_opponent_color_index;
 
         template <int r_dir, int c_dir>
         constexpr auto bit_from_rdir_and_cdir () -> int
@@ -190,105 +190,85 @@ namespace wisdom
         int my_diagonal_threats = 0;
 
         Threats (const Board& board, Color king_color, int king_row, int king_col)
-                : my_board { board }, my_king_color { king_color },
+                : my_board { board }, my_king_color_index { color_index (king_color) },
                 my_king_row { king_row }, my_king_col { king_col }
         {
-            my_opponent_color = color_invert (my_king_color);
+            my_opponent_color_index = color_index (color_invert (king_color));
         }
 
-        constexpr void check_queen_or_rook (int distance, int r_dir, int c_dir, int lane_bit)
+        constexpr bool check_lane_threats (int target_row, int target_col, int lane_bit)
         {
-            int mask = (1 << lane_bit);
-            int target_row = (distance * r_dir) + my_king_row;
-            int target_col = (distance * c_dir) + my_king_col;
-
-            // If the row or col is outside the limits, set the blocked bit
-            // and ignore any calculation.
-            int block_row = (is_valid_row (target_row) << lane_bit);
-            int block_col = (is_valid_column (target_col) << lane_bit);
-            my_blocked_lanes |= block_row | block_col;
-
             ColoredPiece piece = my_board.piece_at (target_row, target_col);
-            Piece type = piece_type (piece);
-            Color target_color = piece_color (piece);
+            int type = piece_index (piece_type (piece));
+            int target_color = color_index (piece_color (piece));
 
             // 1 or 0: whether to consider a piece or revert to the king
             // position.
-            int is_blocked = mask ^ (my_blocked_lanes & mask);
-            int is_rook = type == Piece::Rook;
-            int is_queen = type == Piece::Queen;
-            int is_opponent_color = target_color == my_opponent_color;
+            int is_rook = type == piece_index (Piece::Rook);
+            int is_queen = type == piece_index (Piece::Queen);
+            int is_opponent_color = target_color == my_opponent_color_index;
 
-            int has_piece = (is_rook | is_queen) & is_opponent_color;
+            int has_threatening_piece = (is_rook | is_queen) & is_opponent_color;
 
             // If the check is blocked, revert to the the king position itself
             // for the calculation to avoid any branching.
-            my_lane_threats |= (has_piece << lane_bit) & is_blocked;
+            my_lane_threats |= has_threatening_piece;
+
+            return type != piece_index (Piece::None);
         }
 
-        bool any_lane_threats ()
+        bool any_row_threats ()
         {
-            // check each side of the king's row
-//            int col = my_king_col;
-//            int row;
-//
-//            for (int r_dir = -1; r_dir <= 1; r_dir += 2)
-//            {
-//                for (row = next_row (my_king_row, r_dir); is_valid_row (row); row = next_row (row, r_dir))
-//                {
-//                    auto what = my_board.piece_at (row, col);
-//
-//                    if (what == Piece_And_Color_None)
-//                        continue;
-//
-//                    auto check_piece_type = piece_type (what);
-//                    auto check_piece_color = piece_color (what);
-//
-//                    if (check_piece_color == my_king_color)
-//                        break;
-//
-//                    if (check_piece_type == Piece::Rook ||
-//                        check_piece_type == Piece::Queen)
-//                        return true;
-//
-//                    break;
-//                }
-//            }
-//
-//            // check each side of the king's column
-//            row = my_king_row;
-//            for (int c_dir = -1; c_dir <= 1; c_dir += 2)
-//            {
-//                for (col = next_column (my_king_col, c_dir); is_valid_column (col); col = next_column (col, c_dir))
-//                {
-//                    auto what = my_board.piece_at (row, col);
-//
-//                    if (what == Piece_And_Color_None)
-//                        continue;
-//
-//                    if (piece_color (what) == my_king_color)
-//                        break;
-//
-//                    auto check_piece_type = piece_type (what);
-//                    if (check_piece_type == Piece::Rook ||
-//                        check_piece_type == Piece::Queen)
-//                        return true;
-//
-//                    break;
-//                }
-//            }
-//            return false;
-//
-            // Rook / Queen checks
-            for (int distance = 1; distance <= Num_Rows - 1; distance++)
+            for (int new_col = my_king_col; new_col <= Last_Column; new_col++)
             {
-                check_queen_or_rook (distance, -1, 0, bit_from_rdir_and_cdir<-1, 0>());
-                check_queen_or_rook (distance, 1, 0, bit_from_rdir_and_cdir<1, 0>());
-                check_queen_or_rook (distance, 0, -1, bit_from_rdir_and_cdir<0, -1>());
-                check_queen_or_rook (distance, 0, 1, bit_from_rdir_and_cdir<0, 1>());
+                auto stop = check_lane_threats (my_king_row, new_col, bit_from_rdir_and_cdir<0, 1> ());
+
+                if (my_lane_threats)
+                    return true;
+
+                if (stop)
+                    break;
             }
 
-            return my_lane_threats > 0;
+            for (int new_col = my_king_col; new_col >= First_Column; new_col--)
+            {
+                auto stop = check_lane_threats (my_king_row, new_col, bit_from_rdir_and_cdir<0, -1> ());
+
+                if (my_lane_threats)
+                    return true;
+
+                if (stop)
+                    break;
+            }
+
+            return false;
+        }
+
+        bool any_column_threats ()
+        {
+            for (int new_row = my_king_row + 1; new_row <= Last_Row; new_row++)
+            {
+                auto stop = check_lane_threats (new_row, my_king_col, bit_from_rdir_and_cdir<1, 0> ());
+
+                if (my_lane_threats)
+                    return true;
+
+                if (stop)
+                    break;
+            }
+
+            for (int new_row = my_king_row - 1; new_row >= First_Row; new_row--)
+            {
+                auto stop = check_lane_threats (new_row, my_king_col, bit_from_rdir_and_cdir<-1, 0> ());
+
+                if (my_lane_threats)
+                    return true;
+
+                if (stop)
+                    break;
+            }
+
+            return false;
         }
 
         bool any_diagonal_threats ()
@@ -308,12 +288,12 @@ namespace wisdom
                         if (what == Piece_And_Color_None)
                             continue;
 
-                        if (piece_color (what) == my_king_color)
+                        if (color_index (piece_color (what)) == my_king_color_index)
                             break;
 
-                        auto check_piece_type = piece_type (what);
-                        if (check_piece_type == Piece::Bishop ||
-                            check_piece_type == Piece::Queen)
+                        int check_piece_type = piece_index (piece_type (what));
+                        if (check_piece_type == piece_index (Piece::Bishop) ||
+                            check_piece_type == piece_index (Piece::Queen))
                         {
                             return true;
                         }
@@ -336,7 +316,10 @@ namespace wisdom
         int distance;
         Threats threats { board, who, king_row, king_col };
 
-        if (threats.any_lane_threats ())
+        if (threats.any_row_threats ())
+            return true;
+
+        if (threats.any_column_threats ())
             return true;
 
         if (threats.any_diagonal_threats ())
