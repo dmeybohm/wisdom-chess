24bf832b8439a06c9ecc00b642f7f8af5ed72e5c
David Meybohm <dmeybohm@gmail.com>
2022-02-03 20:43:16 -0500

    Broken down checks

--- engine/benchmark/is_king_threatened.cpp
+++ engine/benchmark/is_king_threatened.cpp
@@ -15,6 +15,7 @@ static void benchmark_is_king_threatened_initial_position (benchmark::State& sta
         benchmark::DoNotOptimize(is_king_threatened (board, Color::White, Last_Row, King_Column));
     }
 }
+BENCHMARK(benchmark_is_king_threatened_initial_position);
 
 static void benchmark_is_king_threatened_initial_position_inline (benchmark::State& state)
 {
@@ -27,6 +28,98 @@ static void benchmark_is_king_threatened_initial_position_inline (benchmark::Sta
         benchmark::DoNotOptimize(is_king_threatened_inline (board, Color::White, Last_Row, King_Column));
     }
 }
+BENCHMARK(benchmark_is_king_threatened_initial_position_inline);
+
+static void benchmark_is_king_threatened_row (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_row (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_row);
+
+static void benchmark_is_king_threatened_column (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_column (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_column);
+
+static void benchmark_is_king_threatened_diagonal (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_diagonal (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_diagonal);
+
+static void benchmark_is_king_threatened_diagonal_dumb (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_diagonal_dumb (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_diagonal_dumb);
+
+static void benchmark_is_king_threatened_knight (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_knight (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_knight);
+
+static void benchmark_is_king_threatened_pawn (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_pawn (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_pawn);
+
+static void benchmark_is_king_threatened_king (benchmark::State& state)
+{
+    using namespace wisdom;
+
+    Board board;
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(is_king_threatened_king (board, Color::White, Last_Row, King_Column));
+    }
+}
+BENCHMARK(benchmark_is_king_threatened_king);
 
 static void benchmark_is_black_king_threatened (benchmark::State& state)
 {
@@ -43,6 +136,7 @@ static void benchmark_is_black_king_threatened (benchmark::State& state)
         benchmark::DoNotOptimize(is_king_threatened (board, Color::White, king_row, king_col));
     }
 }
+BENCHMARK(benchmark_is_black_king_threatened);
 
 static void benchmark_is_white_king_threatened (benchmark::State& state)
 {
@@ -59,8 +153,4 @@ static void benchmark_is_white_king_threatened (benchmark::State& state)
         benchmark::DoNotOptimize(is_king_threatened (board, Color::Black, king_row, king_col));
     }
 }
-
-BENCHMARK(benchmark_is_king_threatened_initial_position);
-BENCHMARK(benchmark_is_king_threatened_initial_position_inline);
-BENCHMARK(benchmark_is_black_king_threatened);
 BENCHMARK(benchmark_is_white_king_threatened);
