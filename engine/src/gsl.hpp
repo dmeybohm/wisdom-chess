9b50a37765a7dde286b1e12657403b003f4463a5
David Meybohm <dmeybohm@gmail.com>
2022-04-18 00:03:28 -0400

    Add gsl.hpp

---
+++ engine/src/gsl.hpp
@@ -0,0 +1,25 @@
+#ifndef WISDOM_CHESS_GSL_HPP
+#define WISDOM_CHESS_GSL_HPP
+
+namespace gsl
+{
+    template <typename T>
+    using not_null = T;
+
+    using zstring = char*;
+    using czstring = const char*;
+
+    template <typename T, typename U>
+    constexpr auto narrow_cast(U anything) -> T
+    {
+        return static_cast<T>(anything);
+    }
+
+    template <typename T, typename U>
+    constexpr auto narrow(U anything) -> T
+    {
+        return static_cast<T>(anything);
+    }
+}
+
+#endif // WISDOM_CHESS_GSL_HPP
