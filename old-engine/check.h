9ec9743658d47950696819fa621430088c63a17a
David Meybohm <dmeybohm@gmail.com>
2022-06-04 12:35:22 -0400

    Remove old-engine dir

--- old-engine/check.h
+++
@@ -1,9 +0,0 @@
-#ifndef EVOLVE_CHESS_CHECK_H
-#define EVOLVE_CHESS_CHECK_H
-
-int    was_legal_move      (struct board *board, color_t who, move_t *mv);
-int    is_king_threatened (struct board *board, color_t who, unsigned char row,
-                           unsigned char col);
-int    is_checkmated      (struct board *board, color_t who);
-
-#endif /* EVOLVE_CHESS_CHECK_H */
