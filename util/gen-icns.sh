cf2d5f963fabd5c620e5664eb032700f140dd5a0
David Meybohm <dmeybohm@gmail.com>
2022-06-12 11:13:30 -0400

    Add script for generating icns file

---
+++ util/gen-icns.sh
@@ -0,0 +1,17 @@
+input_filepath="qt-qml-gui/images/Chess_rlt256.png"
+output_iconset_name="CFE.iconset"
+mkdir $output_iconset_name
+
+sips -z 16 16     $input_filepath --out "${output_iconset_name}/icon_16x16.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_16x16@2x.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_32x32.png"
+sips -z 64 64     $input_filepath --out "${output_iconset_name}/icon_32x32@2x.png"
+sips -z 128 128   $input_filepath --out "${output_iconset_name}/icon_128x128.png"
+sips -z 256 256   $input_filepath --out "${output_iconset_name}/icon_128x128@2x.png"
+sips -z 256 256   $input_filepath --out "${output_iconset_name}/icon_256x256.png"
+sips -z 512 512   $input_filepath --out "${output_iconset_name}/icon_256x256@2x.png"
+sips -z 512 512   $input_filepath --out "${output_iconset_name}/icon_512x512.png"
+
+iconutil -c icns $output_iconset_name
+
+rm -R $output_iconset_name
