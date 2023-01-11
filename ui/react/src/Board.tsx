76a0e7648de7914da00d018a56d7584c9487e4ed
David Meybohm <dmeybohm@gmail.com>
2023-01-11 18:58:55 -0500

    Add white pieces

--- ui/react/src/Board.tsx
+++ ui/react/src/Board.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import Square from "./Square";
 import "./Board.css";
+import WhitePawn from "./assets/Chess_plt45.svg";
+import WhiteBishop from "./assets/Chess_blt45.svg";
+import WhiteKnight from "./assets/Chess_nlt45.svg";
+import WhiteQueen from "./assets/Chess_qlt45.svg";
+import WhiteRook from "./assets/Chess_rlt45.svg";
+import WhiteKing from "./assets/Chess_klt45.svg";
 
 let initialSquares : Array<number> = [];
 for (let i = 0; i < 64; i++) {
@@ -28,6 +34,54 @@ const Board = () => {
                     />
                 );
             })}
+            <div className="piece a2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece b2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece c2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece d2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece e2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece f2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece g2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece h2">
+                <img src={WhitePawn} width={96} height={96} />
+            </div>
+            <div className="piece a1">
+                <img src={WhiteRook} width={96} height={96} />
+            </div>
+            <div className="piece b1">
+                <img src={WhiteKnight} width={96} height={96} />
+            </div>
+            <div className="piece c1">
+                <img src={WhiteBishop} width={96} height={96} />
+            </div>
+            <div className="piece d1">
+                <img src={WhiteQueen} width={96} height={96} />
+            </div>
+            <div className="piece e1">
+                <img src={WhiteKing} width={96} height={96} />
+            </div>
+            <div className="piece f1">
+                <img src={WhiteBishop} width={96} height={96} />
+            </div>
+            <div className="piece g1">
+                <img src={WhiteKnight} width={96} height={96} />
+            </div>
+            <div className="piece h1">
+                <img src={WhiteRook} width={96} height={96} />
+            </div>
         </section>
     )
 }
