ffabd9c44022f21456d6defe36412ef0feab5013
David Meybohm <dmeybohm@gmail.com>
2023-02-08 10:36:19 -0500

    Add WASM_WORKERS api support and fix build dependency

--- ui/react/vite.config.ts
+++ ui/react/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    }
+  }
 })
