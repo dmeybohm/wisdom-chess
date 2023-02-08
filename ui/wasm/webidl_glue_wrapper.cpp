f6fb62a0e8182fa34379b55df792f13f2f1b3f48
David Meybohm <dmeybohm@gmail.com>
2023-02-08 16:55:14 -0500

    Printing message from worker

--- ui/wasm/webidl_glue_wrapper.cpp
+++ ui/wasm/webidl_glue_wrapper.cpp
@@ -1,6 +1,25 @@
+#include <emscripten.h>
+#include <emscripten/wasm_worker.h>
 #include <iostream>
 
 #include "game.hpp"
+
+extern "C"
+{
+
+  EM_JS(void, console_log, (const char* str), {
+    console.log(UTF8ToString(str))
+  })
+
+  EMSCRIPTEN_KEEPALIVE void run_in_worker ()
+  {
+    console_log ("Hello from a worker!\n");
+  }
+}
+
+// The engine thread manager receives messages from the main thread
+emscripten_wasm_worker_t engine_thread_manager;
+emscripten_wasm_worker_t engine_thread;
 
 namespace wisdom
 {
@@ -38,6 +57,7 @@ namespace wisdom
             std::cout << "Setting max depth: " << max_depth << "\n";
             my_game.set_max_depth (max_depth);
       }
+
       auto get_max_depth () -> int
       {
             auto result = my_game.get_max_depth ();
@@ -45,6 +65,13 @@ namespace wisdom
             return result;
       }
 
+      void start_worker ()
+      {
+            std::cout << "Inside start_worker (a bit of a misnomer now?)\n";
+            std::cout << "what? test build\n";
+            emscripten_wasm_worker_post_function_v (engine_thread_manager, run_in_worker);
+            std::cout << "Exiting start_worker\n";
+      }
 
   private:
       Game my_game;
@@ -53,5 +80,12 @@ namespace wisdom
 
 // Map the enum to the global namespace:
 using wisdom_WebPlayer = wisdom::WebPlayer;
+
+int main()
+{
+  // Initialize the worker threads:
+  engine_thread_manager = emscripten_malloc_wasm_worker (/*stack size: */1024);
+  engine_thread = emscripten_malloc_wasm_worker (/*stack size: */1024);
+}
 
 #include "glue.hpp"
--- ui/wasm/wisdom-chess-load.js
+++ ui/wasm/wisdom-chess-load.js
@@ -1,14 +1,68 @@
-importScripts('wisdom-chess-web.js')
+console.log("loading wisdom-chess-web.js")
 
-wisdomChessWeb().then((obj) => {
-	console.log('loaded')
-	let g = new obj.WebGame(obj.Human, obj.ChessEngine);
+// Depending on the build flags that one uses, different files need to be downloaded
+// to load the compiled page. The right set of files will be expanded to be downloaded
+// via the directive below.
+
+function binary(url) { // Downloads a binary file and outputs it in the specified callback
+	return new Promise((ok, err) => {
+		var x = new XMLHttpRequest();
+		x.open('GET', url, true);
+		x.responseType = 'arraybuffer';
+		x.onload = () => { ok(x.response); }
+		x.send(null);
+	});
+}
+
+function script(url) { // Downloads a script file and adds it to DOM
+	return new Promise((ok, err) => {
+		var s = document.createElement('script');
+		s.src = url;
+		s.onload = () => {
+			var c = wisdomChessWeb;
+			delete wisdomChessWeb;
+			ok(c);
+		};
+		document.body.appendChild(s);
+	});
+}
+
+Promise.all([
+	binary('wisdom-chess-web.js'),
+	binary('wisdom-chess-web.wasm'),
+	binary('wisdom-chess-web.ww.js')
+]).then((r) => {
+	// Detour the JS code to a separate variable to avoid instantiating with 'r' array as "this" directly to avoid strict ECMAScript/Firefox GC problems that cause a leak, see https://bugzilla.mozilla.org/show_bug.cgi?id=1540101
+	var js = URL.createObjectURL(new Blob([r[0]], { type: 'application/javascript' }));
+	script(js).then(function(c) {
+		const promise = c({ wasm: r[1],
+		$wb: URL.createObjectURL(new Blob([r[2]], { type: 'application/javascript' })),
+		js: js });
+		promise.then(module => {
+			WisdomChessWeb = module
+			if (afterWisdomChessModuleLoaded) {
+				afterWisdomChessModuleLoaded(WisdomChessWeb)
+			}
+		})
+	});
+});
+
+function afterWisdomChessModuleLoaded(WisdomChessWeb) {
+
+	console.log('after wisdom chess module loaded')	;
+	console.log(WisdomChessWeb)
+	let g = new WisdomChessWeb.WebGame(WisdomChessWeb.Human, WisdomChessWeb.ChessEngine);
 	g.set_max_depth( 10 );
 	console.log(g);
 	console.log(g.get_max_depth());
-})
-
-onmessage = (e) => {
-	console.log('message received');
-	self.postMessage('hello');
+	g.start_worker();
 }
+
+// Hmm, how are those workers going to notify the main thread?
+let cnt = 0;
+onmessage = (e) => {
+	if (cnt % 10000) {
+		console.log('message received', e);
+	}
+}
+
