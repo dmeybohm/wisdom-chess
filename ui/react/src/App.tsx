7d4cd0d32f6d3945077f41fe3adcd58f5bbfae38
David Meybohm <dmeybohm@gmail.com>
2023-02-15 07:53:11 -0500

    Move state to App component

--- ui/react/src/App.tsx
+++ ui/react/src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 import Board from "./Board";
 import TopMenu from "./TopMenu";
 import StatusBar from "./StatusBar";
 import Modal from "./Modal";
+import {initialSquares, Position} from "./Squares";
+import {getPieces, makeGame, WisdomChess} from "./lib/WisdomChess";
 
 function App() {
 
@@ -22,6 +24,66 @@ function App() {
         setShowSettings(true);
     }
 
+    const [squares, setSquares] = useState(initialSquares)
+    const [focusedSquare, setFocusedSquare] = useState('')
+    const [game, setGame] = useState(() => makeGame())
+    const [pieces, setPieces] = useState(() => getPieces(game))
+    const [pawnPromotionDialogSquare, setPawnPromotionDialogSquare] = useState('');
+
+    useEffect(() => {
+        setPieces(getPieces(game))
+    }, [game])
+
+    const wisdomChess : WisdomChess = WisdomChess()
+
+    const findIndexOfPieceAtPosition = (position: string) => {
+        return pieces.findIndex(piece => piece.position === position)
+    }
+
+    //
+    // Either:
+    // - change the focus to the current piece, or
+    // - take the piece if the color of the piece is different
+    //
+    const handlePieceClick = (dstSquare: string) => {
+        setPawnPromotionDialogSquare('')
+        if (focusedSquare === '') {
+            setFocusedSquare(dstSquare)
+            return
+        }
+        if (focusedSquare === dstSquare) {
+            setFocusedSquare('')
+            return
+        }
+        const dstIndex = findIndexOfPieceAtPosition(dstSquare)
+        const srcIndex = findIndexOfPieceAtPosition(focusedSquare)
+        if (dstIndex === -1 || srcIndex === -1 || pieces[dstIndex].color === pieces[srcIndex].color) {
+            setFocusedSquare(dstSquare)
+            return
+        }
+        handleMovePiece(dstSquare)
+        setFocusedSquare('')
+    }
+
+    const handleMovePiece = (dst: string) => {
+        setPawnPromotionDialogSquare('')
+        const src = focusedSquare;
+        if (src === '') {
+            return
+        }
+        // find the piece at the focused square:
+        const srcCoord = wisdomChess.WebCoord.prototype.fromTextCoord(src)
+        const dstCoord = wisdomChess.WebCoord.prototype.fromTextCoord(dst)
+        if (game.needsPawnPromotion(srcCoord, dstCoord)) {
+            setPawnPromotionDialogSquare(dst)
+            return
+        }
+        const movedSuccess = game.makeMove(srcCoord, dstCoord)
+        if (movedSuccess) {
+            setPieces(oldPieces => getPieces(game))
+        }
+    }
+
     return (
         <div className="App">
             <TopMenu
@@ -30,7 +92,14 @@ function App() {
                 settingsClicked={handleSettingsClicked}
             />
             <div className="container">
-                <Board />
+                <Board
+                    squares={squares}
+                    focusedSquare={focusedSquare}
+                    pieces={pieces}
+                    handleMovePiece={handleMovePiece}
+                    handlePieceClick={handlePieceClick}
+                    pawnPromotionDialogSquare={pawnPromotionDialogSquare}
+                />
 
                 <StatusBar />
             </div>
--- ui/react/src/Board.tsx
+++ ui/react/src/Board.tsx
@@ -2,92 +2,40 @@ import React, {useEffect, useState} from 'react'
 import Square from "./Square";
 import "./Board.css";
 import {Piece} from "./Pieces";
-import {initialSquares, Position} from "./Squares";
+import {Position} from "./Squares";
 import "./Positions.css"
-import {getPieces, makeGame, WisdomChess} from "./lib/WisdomChess";
 import PawnPromotionDialog from "./PawnPromotionDialog";
 
-const Board = () => {
-    const [squares, setSquares] = useState(initialSquares)
-    const [focusedSquare, setFocusedSquare] = useState('')
-    const [game, setGame] = useState(() => makeGame())
-    const [pieces, setPieces] = useState(() => getPieces(game))
-    const [pawnPromotionDialogSquare, setPawnPromotionDialogSquare] = useState('');
+export interface BoardProps {
+    squares: Array<Position>
+    focusedSquare: string
+    pieces: Array<Piece>
+    handleMovePiece: (dst: string) => void
+    handlePieceClick: (dst: string) => void
+    pawnPromotionDialogSquare: string
+}
 
-    useEffect(() => {
-        setPieces(getPieces(game))
-    }, [game])
-
-    const wisdomChess : WisdomChess = WisdomChess()
-
-    const findIndexOfPieceAtPosition = (position: string) => {
-        return pieces.findIndex(piece => piece.position === position)
-    }
-
-    //
-    // Either:
-    // - change the focus to the current piece, or
-    // - take the piece if the color of the piece is different
-    //
-    const handlePieceClick = (dstSquare: string) => {
-        setPawnPromotionDialogSquare('')
-        if (focusedSquare === '') {
-            setFocusedSquare(dstSquare)
-            return
-        }
-        if (focusedSquare === dstSquare) {
-            setFocusedSquare('')
-            return
-        }
-        const dstIndex = findIndexOfPieceAtPosition(dstSquare)
-        const srcIndex = findIndexOfPieceAtPosition(focusedSquare)
-        if (dstIndex === -1 || srcIndex === -1 || pieces[dstIndex].color === pieces[srcIndex].color) {
-            setFocusedSquare(dstSquare)
-            return
-        }
-        handleMovePiece(dstSquare)
-        setFocusedSquare('')
-    }
-
-    const handleMovePiece = (dst: string) => {
-        setPawnPromotionDialogSquare('')
-        const src = focusedSquare;
-        if (src === '') {
-            return
-        }
-        // find the piece at the focused square:
-        const srcCoord = wisdomChess.WebCoord.prototype.fromTextCoord(src)
-        const dstCoord = wisdomChess.WebCoord.prototype.fromTextCoord(dst)
-        if (game.needsPawnPromotion(srcCoord, dstCoord)) {
-            setPawnPromotionDialogSquare(dst)
-            return
-        }
-        const movedSuccess = game.makeMove(srcCoord, dstCoord)
-        if (movedSuccess) {
-            setPieces(oldPieces => getPieces(game))
-        }
-    }
-
+const Board = (props: BoardProps) => {
     return (
         <section className="board">
-            {squares.map((position: Position) => {
+            {props.squares.map((position: Position) => {
                 return (
                     <Square
                         key={position.index}
                         isOddRow={position.isOddRow}
                         position={position.position}
-                        onClick={handleMovePiece}
+                        onClick={props.handleMovePiece}
                     />
                 );
             })}
-            {pieces.map((piece: Piece) => (
-                <div className={`piece ${piece.position} ${piece.position === focusedSquare ? 'focused' : ''}`}
+            {props.pieces.map((piece: Piece) => (
+                <div className={`piece ${piece.position} ${piece.position === props.focusedSquare ? 'focused' : ''}`}
                      key={piece.id}
-                     onClick={() => handlePieceClick(piece.position)}>
+                     onClick={() => props.handlePieceClick(piece.position)}>
                     <img alt="piece" src={piece.icon} />
                 </div>
             ))}
-            {pawnPromotionDialogSquare && <PawnPromotionDialog square={pawnPromotionDialogSquare} direction={-1} />}
+            {props.pawnPromotionDialogSquare && <PawnPromotionDialog square={props.pawnPromotionDialogSquare} direction={-1} />}
         </section>
     )
 }
