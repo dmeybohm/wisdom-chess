f6f1dd606652bfb7b31e43084d2b6fbae1fa461f
David Meybohm <dmeybohm@gmail.com>
2023-03-02 09:28:12 -0500

    Sketch out messaging interface

--- ui/wasm/bindings.hpp
+++ ui/wasm/bindings.hpp
@@ -11,9 +11,17 @@
 extern "C"
 {
     void worker_initialize_game();
+
     void start_search();
+
     void main_thread_receive_move (int packed_move);
+
     void worker_receive_move (int packed_move);
+
+    void worker_receive_settings (int whitePlayer, int blackPlayer,
+                                  int thinkingTime, int searchDepth);
+
+    void worker_manager_pause_worker (void);
 }
 
 #endif // WISDOMCHESS_BINDINGS_HPP
--- ui/wasm/web_worker.hpp
+++ ui/wasm/web_worker.hpp
@@ -3,6 +3,9 @@
 
 #include "web_types.hpp"
 #include "game_settings.hpp"
+#include "bindings.hpp"
+
+#include <emscripten/wasm_worker.h>
 
 namespace wisdom
 {
@@ -11,28 +14,41 @@ namespace wisdom
     private:
         GameSettings my_game_settings {};
 
-    public:
-        // Initialize a new game with the default position.
-        void sendNewGame()
-        {
-
-        }
-
         // Send new settings to the worker.
         void sendSettings (GameSettings settings)
         {
+            int whitePlayer = static_cast<int> (settings.whitePlayer);
+            int blackPlayer = static_cast<int> (settings.blackPlayer);
+
+            emscripten_wasm_worker_post_function_sig (engine_thread,
+                                                      (void*)worker_receive_settings,
+                                                      "iiii",
+                                                      whitePlayer, blackPlayer,
+                                                      settings.thinkingTime, settings.searchDepth);
 
         }
+
+    public:
+        // Initialize a new game with the default position.
+        void sendNewGame (int newGameId)
+        {
+
+        }
+
 
         // Pause the worker.
         void sendPause()
         {
+            emscripten_wasm_worker_post_function_v (engine_thread_manager,
+                                                    worker_manager_pause_worker);
         }
 
         // Resume the worker.
         void sendResume()
         {
 
+            emscripten_wasm_worker_post_function_v (engine_thread_manager,
+                                                    worker_manager_resume_worker);
         }
 
         auto getCurrentGameSettings() -> GameSettings*
@@ -42,7 +58,7 @@ namespace wisdom
 
         void setCurrentGameSettings (GameSettings* newSettings)
         {
-            my_game_settings  = GameSettings { *newSettings };
+            my_game_settings = GameSettings { *newSettings };
             sendPause ();
             sendSettings (my_game_settings);
             sendResume ();
