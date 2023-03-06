706f5cb76496d8d92ed3a8a87cefd82f6bd8c1d5
David Meybohm <dmeybohm@gmail.com>
2023-03-05 23:19:50 -0500

    Rename additional usages of was_legal_move

--- engine/test/perft.cpp
+++ engine/test/perft.cpp
@@ -25,7 +25,7 @@ namespace wisdom
         {
             Board new_board = board.with_move (side, move);
 
-            if (!was_legal_move (new_board, side, move))
+            if (!is_legal_position_after_move (new_board, side, move))
                 continue;
 
             if (depth == target_depth)
@@ -164,10 +164,8 @@ namespace wisdom
             Color next_player = wisdom::color_invert (active_player);
             auto new_board = board.with_move (active_player, move);
 
-            if (!wisdom::was_legal_move (new_board, active_player, move))
-            {
+            if (!wisdom::is_legal_position_after_move (new_board, active_player, move))
                 continue;
-            }
 
             stats.search_moves (new_board, next_player, 1, depth, generator);
 
