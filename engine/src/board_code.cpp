830d261346c3f7ae5ce7cd902d33bce54d1099d4
David Meybohm <dmeybohm@gmail.com>
2024-03-13 21:15:48 -0400

    Remove references

--- engine/src/board_code.cpp
+++ engine/src/board_code.cpp
@@ -11,7 +11,7 @@ namespace wisdom
     {
         auto iterator = Board::allCoords();
         my_code = std::accumulate (iterator.begin(), iterator.end(), my_code,
-                                   [](const auto& value, const auto& coord) {
+                                   [](const auto value, const auto coord) {
                                        return value ^ boardCodeHash (coord, Piece_And_Color_None);
                                    });
     }
