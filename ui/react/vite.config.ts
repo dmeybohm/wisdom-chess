85dc29146378fd6ab094c8a81af172d7056a9a68
David Meybohm <dmeybohm@gmail.com>
2024-04-12 19:59:08 -0400

    Add PWA Vite plugin

--- ui/react/vite.config.ts
+++ ui/react/vite.config.ts
@@ -1,9 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo-192x192.png', 'logo-512x512.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wasm}'],
+        maximumFileSizeToCacheInBytes: 10000000
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Wisdom Chess',
+        short_name: 'WisdomChess',
+        description: 'Wisdom Chess',
+        theme_color: '#c7c7c7',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
