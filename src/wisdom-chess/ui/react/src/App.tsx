31f6d9272bc98b706890af19fc75383279ebe560
David Meybohm <dmeybohm@gmail.com>
2025-10-18 06:54:38 -0400

    Add tests for reducer and components

--- src/wisdom-chess/ui/react/src/App.tsx
+++ src/wisdom-chess/ui/react/src/App.tsx
@@ -26,55 +26,7 @@ import {
 } from './lib/WisdomChess'
 import Modal from "./Modal";
 import {initialSquares} from "./lib/Squares";
-
-type Action =
-    | { type: 'BOOTSTRAP'; snapshot: Partial<GameState> }
-    | { type: 'ENGINE_SYNC'; snapshot: Partial<GameState> }
-    | { type: 'FOCUS'; square: string }
-    | { type: 'CLEAR_FOCUS' }
-    | { type: 'REQUEST_PROMOTION'; src: string; dst: string }
-    | { type: 'SET_SETTINGS'; settings: WebGameSettings }
-    | { type: 'SET_LAST_DROPPED'; square: string }
-
-function reducer(state: GameState, action: Action): GameState {
-    switch (action.type) {
-        case 'BOOTSTRAP':
-        case 'ENGINE_SYNC':
-            return { ...state, ...action.snapshot }
-
-        case 'FOCUS':
-            return {
-                ...state,
-                focusedSquare: action.square,
-                pawnPromotionDialogSquare: '',
-                lastDroppedSquare: '',
-            }
-
-        case 'CLEAR_FOCUS':
-            return {
-                ...state,
-                focusedSquare: '',
-                pawnPromotionDialogSquare: '',
-                lastDroppedSquare: '',
-            }
-
-        case 'REQUEST_PROMOTION':
-            return {
-                ...state,
-                focusedSquare: action.src,
-                pawnPromotionDialogSquare: action.dst,
-            }
-
-        case 'SET_SETTINGS':
-            return { ...state, settings: { ...action.settings } }
-
-        case 'SET_LAST_DROPPED':
-            return { ...state, lastDroppedSquare: action.square }
-
-        default:
-            return state
-    }
-}
+import { reducer } from './reducer'
 
 function toWebSettings(wasm: any): WebGameSettings {
     return {
